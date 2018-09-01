8f14fc893723f068bfe6b56a618a4251507bbaea
Manish Sharma <31279522+mnsgeeks@users.noreply.github.com>
2018-09-01 12:22:35 +0530

    AI problem

---
+++ AI.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+void rescue(int n,char* s)
+{
+ 
+if(s[0][0]=='p')
+{
+  for(int i=0;i<n/2;i++)
+   {  cout<<"LEFT"<<endl; }
+ for(int j=0;j<n/2;j++)
+   { cout<<"UP"<<endl; }
+   
+ }
+
+if(s[0][(n/2)*2]=='p')
+{
+  for(int i=0;i<n/2;i++)
+   {  cout<<"RIGHT"<<endl; }
+ for(int j=0;j<n/2;j++)
+   { cout<<"UP"<<endl; }
+}
+if(s[(n/2)*2][0]=='p')
+{
+ for(int i=0;i<n/2;i++)
+   {  cout<<"DOWN"<<endl; }
+ for(int j=0;j<n/2;j++)
+   { cout<<"LEFT"<<endl; }
+
+}
+if(s[(n/2)*2][(n/2)*2]=='p')
+{
+ for(int i=0;i<n/2;i++)
+   {  cout<<"DOWN"<<endl; }
+ for(int j=0;j<n/2;j++)
+   { cout<<"RIGHT"<<endl; }
+
+}
+
+
+
+
+}
+
+
+int main()
+{
+  int m; cin>>m;
+  char s[m][m];
+  for(int i=0;i<m;i++) {
+    for(int j=0;j<m;j++) {
+       cin>>s[i][j]; }   }
+
+
+rescue(m,s);
+
+return 0;
+}
+
+
+
+
+
+
