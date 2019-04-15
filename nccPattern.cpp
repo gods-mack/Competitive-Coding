f1d4508a3480bc63fd318a10f657e71d8521c9e7
Manish Sharma <31279522+gods-mack@users.noreply.github.com>
2019-04-15 22:46:41 +0530

    ncc pattern of AMCAT test

--- nccPattern.cpp
+++ nccPattern.cpp
@@ -16,32 +16,34 @@
 #include<iostream>
 using namespace std;
 int main(){
- int n;
- cin>>n;
- int midCount=1;
+    int n;
+    cin>>n;
+    int midCount=1;
 
 for(int i=1;i<=n+1;i++)
 {
 
- if(i==1){
-  for (int j=1;j<=n;j++){
-   cout<<n;
-  }
- }
- 
- else{
-   for(int k=1;k<=n;k++){ 
-     
-     if(k!=(n+1)/2){
-     cout<<n;
-     }
-     else{ 
-       cout<<midCount;
-       midCount += 1;
-     
-     }
+    if(i==1){
+        for (int j=1;j<=n;j++){
+           cout<<n;
+        } 
     }
- }
+  
+ 
+    else {
+
+          for(int k=1;k<=n;k++){ 
+     
+             if(k!=(n+1)/2){
+                cout<<n;
+             }
+             else{ 
+               cout<<midCount;
+               midCount += 1;
+     
+             }
+          }    
+     }
  cout<<"\n";
 }
 
