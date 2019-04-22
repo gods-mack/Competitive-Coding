9d2a7f9afb1b325abf994a8f7b7f7f836679e5c4
Manish Sharma <31279522+gods-mack@users.noreply.github.com>
2019-04-22 23:29:27 +0530

    Update nannakuPrematho.cpp

--- nannakuPrematho.cpp
+++ nannakuPrematho.cpp
@@ -7,74 +7,90 @@ All people do the same until only 1 survives. Who will survive at the last?
 using namespace std;
 
 struct node
-{ int data; 
-  node *next;
- };
+{  int data; 
+   node *next;
+};
 
 class List
 {
-  public:
-  node *h,*t;
-  List() { h=NULL; t=NULL; }
+   public:
+   node *h,*t;
+   List() { h=NULL; t=NULL; }
    
- void insert(int data)
- { node *n=new node();
-   n->data=data;
-   if(h==NULL)
-  { h=n;t=n; }
-  else
- { 
-   t->next=n; 
-   t=n;
- }
- if(data==100)
-  { t->next=h; }
+   void insert(int data)
+ {
+     node *n=new node();
+     n->data=data;
+     if(h==NULL)
+     { 
+        h=n;t=n;
+     }
+     
+    else
+   { 
+      t->next=n; 
+      t=n;
+   }
+     
+   if(data==100)
+   {
+      t->next=h;
+   }
 
  }
 
   void killProcess()
- { kill(h); }
- void kill(node *root )
- { node *crnt=root;
+  { 
+    kill(h);
+  }
+ 
+   void kill(node *root )
+  {
+     node *crnt=root;
     
-    while(1) 
-    {
+     while(1) 
+     {
      
   
-   crnt->next=crnt->next->next;
+         crnt->next=crnt->next->next;
     
-  crnt=crnt->next;
-  root=crnt;
-  if(root->next==root->next->next)
-  { 
-   cout<<root->data<<"  bachh gya sala "<<endl;
-   break;
-  }
-   }
+         crnt=crnt->next;
+         root=crnt;
+        if(root->next==root->next->next)
+        { 
+           cout<<root->data<<"  bachh gya sala "<<endl;
+           break;
+        }
+     }
   //cout<<root->data;
  
-  }
+  } // ending of kill function
   
  void print()
-  { node *tmp; 
-   tmp=h;
-  while(tmp!=NULL)
-  { cout<<tmp->data<<" ";
-    tmp=tmp->next;
-  }
+  { 
+     node *tmp; 
+     tmp=h;
+   
+     while(tmp!=NULL)
+    { 
+       cout<<tmp->data<<" ";
+       tmp=tmp->next;
+    }
 
-  }
+ }
  
 
 
-};
+};  // ending of class
 
 
 int main()
 {
   List a;
   for(int i=1;i<=100;i++)
-  { a.insert(i); }
+  {
+    a.insert(i);
+  }
   
  a.killProcess();
  
