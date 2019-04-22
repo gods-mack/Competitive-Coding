d04d81411e9bb3c040a1daa52cd84a6f33ea97db
Manish Sharma <31279522+gods-mack@users.noreply.github.com>
2019-04-22 23:24:28 +0530

    south movie problem

--- nannakuPrematho.cpp
+++ nannakuPrematho.cpp
@@ -1,4 +1,6 @@
-/* 100 people standing in a circle in an order 1 to 100. No. 1 has a sword. He kills   the next person (i.e. No. 2) and gives the sword to the next (i.e. No. 3). All people do the same until only 1 survives. Who will survive at the last?
+/* 100 people standing in a circle in an order 1 to 100. No. 1 has a sword.
+He kills   the next person (i.e. No. 2) and gives the sword to the next (i.e. No. 3).
+All people do the same until only 1 survives. Who will survive at the last?
  */
 
 #include<iostream>
