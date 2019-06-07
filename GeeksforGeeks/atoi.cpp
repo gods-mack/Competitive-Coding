11ee000c30e0d2aec4369a1d9d7805c0a77744e0
Manish Sharma <31279522+gods-mack@users.noreply.github.com>
2019-06-07 23:23:22 +0530

    atoi function

---
+++ GeeksforGeeks/atoi.cpp
@@ -0,0 +1,63 @@
+// implemention of atoi function
+// atoi = convert sring into integer
+// string may contain char,special char(=,+,&,*), integers  etc.
+
+// eg string = "2836sm45d"  OUTPUT--> 283645
+
+#include<iostream>
+#include<cstring>
+#include<cstdlib>
+#include<bits/stdc++.h>
+#include<cmath>
+
+using namespace std;
+
+
+string extractString(string s){
+	
+	string tmp;
+	
+	for(int i=0;i<s.length();i++){
+		if(int(s[i])>=48 and int(s[i])<=57){
+			tmp =  tmp+s[i];
+		}
+    } 
+	
+	return tmp;
+}
+	
+
+int makeAtoi(string s){
+
+	int size = s.length()-1;
+	int answer = 0;
+	int tmpSize  = size+1;
+	
+	
+	
+	
+	for(int i=0;i<tmpSize;i++){
+		
+		    answer = answer + ( (int(s[i])-48)* pow(10,size) );
+			cout<<answer<<endl;
+			size--;
+	}
+	
+	return answer;
+}
+		
+
+
+int main(){
+	
+	string s;
+	cin>>s;
+	
+	int result = makeAtoi(extractString(s));
+	
+	cout<<"atoi string: "<<result;
+	
+	
+	
+}
+
