2ab69227a41e384ef3f15e52a96c7bfc1c3f8ef1
Manish Sharma <31279522+gods-mack@users.noreply.github.com>
2019-07-05 02:37:46 +0530

    Next larger Element

--- Flipkart/next_large_element.cpp
+++ Flipkart/next_large_element.cpp
@@ -34,12 +34,12 @@ int main()
 	    
 	    for(int i = 0; i < n; i++){
 	            cin >> arr[i];
-      }
+                    }
 	    stack <ll> s;
 	    for(int i = n-1; i >= 0; i--) {
 	        while(!s.empty() && arr[i] >= s.top()){
 	            s.pop();
-           }
+          	    }
 	        ans[i] = s.empty() ? -1 : s.top();
 	        s.push(arr[i]);
 	    }
