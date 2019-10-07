7d510fc656c952db136e3c77d9eb6f0f32d81ecb
gods-mack <sehorem@gmail.com>
2019-10-07 14:42:29 +0530

    longest common subseq.

--- Dynamic_programming/lcsBottomUp.cpp
+++ Dynamic_programming/lcsBottomUp.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 
 
+
 int lcs(string s1, string s2, int m, int n){
 
 	int memo[m+1][n+1];
@@ -27,10 +28,38 @@ int lcs(string s1, string s2, int m, int n){
 		}
 	}
 
-	cout<<"answer "<<memo[m][n];
+	// back tracking
+	int index = memo[m][n];
+	char lcs[index+1];
+	lcs[index] = '\0';
+	int i = m, j = n; 
+   while (i > 0 && j > 0) 
+   { 
+      // If current character in X[] and Y are same, then 
+      // current character is part of LCS 
+      if (s1[i-1] == s2[j-1]) 
+      { 
+          lcs[index-1] = s1[i-1]; // Put current character in result 
+          i--; j--; index--;     // reduce values of i, j and index 
+      } 
+  
+      // If not same, then find the larger of two and 
+      // go in the direction of larger value 
+      else if (memo[i-1][j] > memo[i][j-1]) 
+         i--; 
+      else
+         j--; 
+   } 
+
+	
+	cout<<lcs;
+	cout<<endl;
 	return memo[m][n];
 
+
 }
+
+
 
 int main(){
 	string s1;
