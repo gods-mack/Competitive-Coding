3405fcc125255216cc0f66b37931ff9565913057
gods-mack <sehorem@gmail.com>
2019-10-15 22:36:02 +0530

    minimum path sum

---
+++ Dynamic_programming/minPathSum.cpp
@@ -0,0 +1,80 @@
+/*
+Given a N X N  matrix Matrix[N][N] of positive integers.  There are only three possible moves 
+from a cell Matrix[r][c].
+
+1. Matrix[r+1][c]
+
+2. Matrix[r+1][c-1]
+
+3. Matrix[r+1][c+1]
+
+Starting from any column in row 0, return the largest sum of any of the paths up to row N-1.
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// Top Down  solution
+int minPathSum(vector<vector<int> > grid, int x, int y, int N){
+
+	if(x>N or  y>N){
+		return INT_MAX;
+
+	}
+	else if(x==N and y==N){
+		//cout<<"tha pe "<<grid[x][y]<<endl;
+		return grid[x][y];
+
+	}
+	else{
+		return grid[x][y] + min(min(minPathSum(grid,x+1,y,N),minPathSum(grid,x,y+1,N)),minPathSum(grid,x+1,y+1,N));
+	}
+}
+
+// bottom up
+int bottomUp(vector<vector<int> > grid,  int N){
+	int memo[N][N];
+	memo[0][0] = grid[0][0];
+
+	for(int i = 1; i < N; i++){ // intialize first row
+		memo[0][i] = grid[0][i] + memo[0][i-1];
+	}
+
+	for(int i = 1; i < N; i++){ // intialize first column
+		memo[i][0] = grid[i][0] + memo[i-1][0];
+	}
+
+	for(int i = 1; i < N; i++){
+		for(int j = 1; j  < N; j++){
+			memo[i][j] = grid[i][j] +  min(min(memo[i-1][j],memo[i][j-1]),memo[i-1][j-1]);
+		}
+	}
+
+	return memo[N-1][N-1];
+
+
+}
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		vector<vector<int> > grid(n);
+		for(int i = 0; i < n; i++){
+			vector<int> tmp;
+			for(int j = 0; j < n; j++ ){
+				int x;
+				cin>>x;
+				tmp.push_back(x);
+			}
+			grid[i] = tmp;
+		}
+		cout<<"cost "<<minPathSum(grid,0,0,n-1)<<endl;
+		cout<<"cost bottom up "<<bottomUp(grid,n)<<endl;
+	}
+}
