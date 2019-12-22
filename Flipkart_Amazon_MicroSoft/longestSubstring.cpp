2c1ac02b24e1c25a3d406a692048ffb70762e251
gods-mack <sehorem@gmail.com>
2019-12-22 19:18:56 +0530

    longest substring without repeating any character

--- Flipkart_Amazon_MicroSoft/longestSubstring.cpp
+++ Flipkart_Amazon_MicroSoft/longestSubstring.cpp
@@ -1,4 +1,11 @@
+/*
+Given a string, find the length of the longest substring without repeating characters. 
 
+For example, 
+the longest substrings without repeating characters 
+for “ABDEFGABEF” are “BDEFGA” and “DEFGAB”, with length 6.
+
+*/
 
 #include<bits/stdc++.h>
 using namespace std;
