7893f12f8870d31927825c628b111e073cc934e3
gods-mack <manish-1004@outlook.com>
2020-04-21 23:15:08 +0530

    added bottom up approach solution

--- Dynamic_programming/subSetSumTopDown.cpp
+++ Dynamic_programming/subSetSumTopDown.cpp
@@ -2,6 +2,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+// TOPDOWN
 bool sumSetSum(vector<int> a , int sum, int index){
 
 	if(sum == 0){
@@ -19,6 +21,46 @@ bool sumSetSum(vector<int> a , int sum, int index){
 	}
 	
 } 
+
+
+// bOTTOM UP
+bool bottom_up(vector<int> a, int sum){
+	
+	int n = a.size();
+	bool dp[n+1][sum+1] ;
+	
+	
+	
+	for(int i = 0; i <= n; i++){
+		dp[i][0] = true;
+	}
+	
+	for(int i = 1; i <= sum; i++){
+		dp[0][i] = false;
+	}
+	
+	for(int i = 1; i <= n; i++){
+		
+		for(int j = 1; j <= sum; j++){
+			
+			if(j < a[i-1]){
+				dp[i][j] = dp[i-1][j];
+			}
+
+			if(j >= a[i-1]){
+				dp[i][j] = dp[i-1][j] or dp[i-1][j-a[i-1]] ;
+			}
+			
+				
+		}
+	}
+	
+	
+	
+	return dp[n][sum];
+	
+}
+
 
 int main(){
 	int n;
