8878f0aba0da5a8e70c2ebf5d2775d5728df707b
gods-mack <manish-1004@outlook.com>
2020-12-22 15:35:55 +0530

    Diagonal Traversal of tree

---
+++ leetNgeeks/450/bin_tree/diagonal_traversal.cpp
@@ -0,0 +1,36 @@
+
+
+
+
+
+void diag_traversal_util(Node *root,int d,map<int,vector<int>> mp) {
+
+
+	if(root == nullptr) {
+		return;
+	}
+
+	mp[d].push_back(root->data);
+
+	diag_traversal_util(root->left,d+1,mp); // incresase vertical distance left side 
+
+
+	diag_traversal_util(root->right,d,mp); // right side 'd' will be same or slope same
+
+}
+
+
+void diag_traversal(Node *root) {
+
+	vector<int> ans;
+	if(root == nullptr) {
+		return;
+	}
+
+
+	map<int,vector<int>> mp;
+
+	diag_traversal_util(root,0,mp);
+
+
+}
